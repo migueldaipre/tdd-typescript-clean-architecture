381cd845287868c460b70c68e287be6c9bf2f880
Miguel Daipre <contato@migueldaipre.dev>
2020-07-30 21:17:28 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '!<rootDir>/src/**/*-protocols.ts',
     '!**/protocols/**'
   ],
+  preset: '@shelf/jest-mongodb',
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
